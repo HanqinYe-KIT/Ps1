21e8a82a625f6daf1770e06913007e1e74baa81a
Hanqin Ye <Yehanqin999@gmail.com>
2025-05-13 06:54:47 +0800

    Aufgabe 1.4: Pythagorean triple mit a+b+c=1000 gefunden

--- Ps1.cpp
+++ Ps1.cpp
@@ -1,27 +1,17 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 计算最大公约数（欧几里得算法）
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-// 计算最小公倍数
-int lcm(int a, int b) {
-    return (a * b) / gcd(a, b);
-}
-
 int main() {
-    int result = 1;
-    for (int i = 2; i <= 20; ++i) {
-        result = lcm(result, i);
+    for (int a = 1; a < 1000; ++a) {
+        for (int b = a + 1; b < 1000 - a; ++b) {
+            int c = 1000 - a - b;
+            if (a * a + b * b == c * c) {
+                cout << "a = " << a << ", b = " << b << ", c = " << c << endl;
+                cout << "a * b * c = " << a * b * c << endl;
+                return 0;
+            }
+        }
     }
-    cout << "Kleinste Zahl, die durch 1 bis 20 ohne Rest teilbar ist: " << result << endl;
     return 0;
 }
 
