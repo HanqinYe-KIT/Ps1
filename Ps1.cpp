abb679542f880cebb556f119665e9c65aae7a92b
Hanqin Ye <Yehanqin999@gmail.com>
2025-05-13 06:56:30 +0800

    Aufgabe 1.6: Muenzwurf 1000 Mal simuliert

--- Ps1.cpp
+++ Ps1.cpp
@@ -4,40 +4,27 @@
 using namespace std;
 
 int main() {
-    const int SIZE = 10;
-    int numbers[SIZE];
+    const int NUM_TRIALS = 1000;
+    int kopf_count = 0;
+    int zahl_count = 0;
 
-    // 1. 初始化随机数种子
+    // 初始化随机数种子
     srand(time(0));
 
-    // 2. 生成 10 个 0~99 的随机数，填入数组
-    for (int i = 0; i < SIZE; ++i) {
-        numbers[i] = rand() % 100;
-    }
-
-    // 3. 输出数组内容
-    cout << "Zahlen im Array: ";
-    for (int i = 0; i < SIZE; ++i) {
-        cout << numbers[i] << " ";
-    }
-    cout << endl;
-
-    // 4. 查找最大值
-    int max_number = numbers[0];
-    for (int i = 1; i < SIZE; ++i) {
-        if (numbers[i] > max_number) {
-            max_number = numbers[i];
+    // 1000 次投掷
+    for (int i = 0; i < NUM_TRIALS; ++i) {
+        int result = rand() % 2;  // 0 或 1
+        if (result == 0) {
+            ++kopf_count;
+        }
+        else {
+            ++zahl_count;
         }
     }
-    cout << "Groesste Zahl: " << max_number << endl;
 
-    // 5. 计算平均值
-    int sum = 0;
-    for (int i = 0; i < SIZE; ++i) {
-        sum += numbers[i];
-    }
-    double average = static_cast<double>(sum) / SIZE;
-    cout << "Mittelwert: " << average << endl;
+    // 结果输出
+    cout << "Kopf wurde " << kopf_count << " mal geworfen." << endl;
+    cout << "Zahl wurde " << zahl_count << " mal geworfen." << endl;
 
     return 0;
 }
