55351cf58e7a7e098b1907ee0aeee860cd15fa51
Hanqin Ye <Yehanqin999@gmail.com>
2025-05-13 06:51:17 +0800

    Aufgabe 1.3: LCM von 1 bis 20 berechnet

--- Ps1.cpp
+++ Ps1.cpp
@@ -1,8 +1,27 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
+// 计算最大公约数（欧几里得算法）
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// 计算最小公倍数
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
 int main() {
-    cout << "Hallo, Programmiersprachenkurs!" << endl;
+    int result = 1;
+    for (int i = 2; i <= 20; ++i) {
+        result = lcm(result, i);
+    }
+    cout << "Kleinste Zahl, die durch 1 bis 20 ohne Rest teilbar ist: " << result << endl;
     return 0;
 }
+
